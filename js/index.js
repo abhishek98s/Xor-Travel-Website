cf1903b1f8843339154befca282fe0a0133e15df
abhsihek <abhishek_mi@yonefu.info>
2023-06-16 09:36:59 +0545

    changes

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(".close").click(function () {
+    $(".mobile_nav_box_none").toggleClass("mobile_nav_box");
+});
+
+$(".menu").click(function () {
+    $(".mobile_nav_box_none").toggleClass("mobile_nav_box");
+});
